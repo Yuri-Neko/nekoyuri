28d1b273a41c65a7a3c07c3ad85e91738f28230c
NekoKyun <91820453+FjrIndex@users.noreply.github.com>
2021-11-28 13:58:10 +0700

    Create NekoVideo.js

---
+++ shop/NekoVideo.js
@@ -0,0 +1,23 @@
+const list_valo = (prefix) => {
+return `*ZEEONE OFC さ*
+		
+PROSES	1-180Mnt Max 1X24 Jam		
+OPEN	10.00-19.00		
+FORMAT	ID		
+			
+VALORANT POINT			
+		
+•NTR	0 	
+•YAOI	0		
+•YURI	0	
+•ELF	0		
+•MILF	0		
+•HAREM  0		
+•LOLI	0		
+			
+Untuk pay / pembayan silahkan ketik ${prefix}pay
+`
+	}
+
+exports.list_valo = list_valo
+ 
