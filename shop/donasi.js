1aeba01deebafe0a042a4df1c7bda983da640bb7
NekoKyun <91820453+FjrIndex@users.noreply.github.com>
2021-11-28 13:50:16 +0700

    Update donasi.js

--- shop/donasi.js
+++ shop/donasi.js
@@ -4,24 +4,24 @@ return `DONASI BISA VIA BANK / E-MONEY
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ --- ---- ---- -----
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ --- ---- ---- -----
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ --- ---- ---- -----
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ --- ---- ---- -----
 	
 *💰 E-MONEY*
 
-1. GOPAY
-	ㅁ 0887 4350 47326
+1. SHOPEEPAY
+         • Chat Owner Klo Mau Donasi
 2. OVO
-	ㅁ 0887 4350 47326
+	 • Chat Owner Klo Mau Donasi
 3. DANA
-	ㅁ 0887 4350 47326
+	 • Chat Owner Klo Mau Donasi
 	
-Semuanya atas nama RI****DI
+Semuanya atas nama NekoPyun >///<
 `
 	}
 
