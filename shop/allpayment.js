8f85b13d0098ce776f2135ded04f1a2a5495b0d4
NekoKyun <91820453+FjrIndex@users.noreply.github.com>
2021-11-28 13:51:29 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -4,28 +4,28 @@ return `PAYMENT BY ZEEONE OFC
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ 
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ 
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ 
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ 
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ 
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ 
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ 
 	
 Semuanya atas nama RI****DI
 
 *📲 PULSA*
 
-Tidak tersedia
+Chat Owner
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
